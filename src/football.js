da33f1d9324d1aa9946d0f87caee0126f3b55978
David Hatch <dhatch387@gmail.com>
2015-10-12 00:39:31 -0400

    Messy reservation implementation.

--- src/football.js
+++ src/football.js
@@ -33,13 +33,41 @@ function placeTicketOrder(numberOfTickets, seatSection, studentAffiliation, tick
     throw new Error('invalid seat section');
   }
 
+  var available = ticketBooth[seatSection].available;
+  if (ticketBooth[seatSection].reserved) {
+    available = ticketBooth[seatSection].available;
+    for (var reservation in ticketBooth[seatSection].reserved) {
+      if (reservation !== studentAffiliation) {
+        available -= ticketBooth[seatSection].reserved[reservation];
+      }
+    }
+  }
+
   // Are enough seats available?
-  if (numberOfTickets < ticketBooth[seatSection].available) {
+  if (numberOfTickets <= available) {
+
+    // Are they from a reservation?
+    if (ticketBooth[seatSection].reserved &&
+        ticketBooth[seatSection].reserved[studentAffiliation] > 0) {
+      // Fullfill from reservation
+      var remaining = ticketBooth[seatSection].reserved[studentAffiliation];
+      remaining -= numberOfTickets;
+      if (remaining <= 0) {
+        totalPrice = (numberOfTickets + remaining) * seatPrices[seatSection] * 0.5;
+        numberOfTickets = remaining * -1;
+        remaining = 0;
+      } else {
+        numberOfTickets = 0;
+        // Reserved tickets are half off
+        totalPrice = numberOfTickets * seatPrices[seatSection] * 0.5;
+      }
+      ticketBooth[seatSection].reserved[studentAffiliation] = remaining;
+    }
 
     // If so, decrement the number of tickets available, we just sold 'em!
     ticketBooth[seatSection].available -= numberOfTickets;
     ticketBooth[seatSection].filled += numberOfTickets;
-    totalPrice = numberOfTickets * seatPrices[seatSection];
+    totalPrice += numberOfTickets * seatPrices[seatSection];
 
     // Apply a 50% discount for Yale College students
     if (studentAffiliation === 'yc') {

b081f9b87872b339e50e53f89eaeaac33c5493bc
David Hatch <dhatch387@gmail.com>
2015-10-12 12:17:23 -0400

    Update tests to include scaffolding.

--- tests/footballTests.js
+++ tests/footballTests.js
@@ -23,12 +23,7 @@ describe("Ticket booth", function() {
   });
 
   it("should have 500 seats in A, 500 in B", function() {
-    var limits = {a: 500, b: 500};
-    for (var section in limits) {
-      var totalSeatsInSection =
-        ticketBooth[section].filled + ticketBooth[section].available;
-      expect(totalSeatsInSection).toEqual(limits[section]);
-    }
+    // TODO enter your test code here.
   });
 
   it("should have fewer seats available after selling one", function() {
@@ -82,16 +77,30 @@ describe("Ticket orders (depending on constraints)", function() {
     ticketBooth = createTicketBooth();
   });
 
+  it("should cost $50 for seats in A, $30 for seats in B", function () {
+    var sectionA = placeTicketOrder(1, 'a', null, ticketBooth);
+    var sectionB = placeTicketOrder(1, 'b', null, ticketBooth);
+    expect(sectionA.fulfilled).toBe(true);
+    expect(sectionB.fulfilled).toBe(true);
+    expect(sectionA.totalPrice).toBe(50);
+    expect(sectionB.totalPrice).toBe(30);
+  });
+
+  it("should be half price for Yale College students", function() {
+    // TODO enter your test code here
+  });
+
+// STRETCH Goals
+  it("should have a block of 100 half-price reserved for SOM students", function() {
+    // TODO (strech) enter your test code here
+  });
+
+  it("should properly handle purchases fullfilled partially from reserved seats", function() {
+    // TODO (stretch) enter your test code here
+  });
+
   it("should not let non-SOM students purchase reserved seats", function() {
     var orderResults = placeTicketOrder(500, 'b', null, ticketBooth);
     expect(orderResults.fulfilled).toBe(false);
   });
-
-  it("should decrement seats available", function() {
-    var orderResults = placeTicketOrder(1, 'a', 'yc', ticketBooth);
-    expect(orderResults.totalPrice).toBe(50/2);
-    orderResults = placeTicketOrder(1, 'b', 'yc', ticketBooth);
-    expect(orderResults.totalPrice).toBe(30/2);
-  });
-
 });
